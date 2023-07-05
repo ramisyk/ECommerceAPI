d3a32533b3fc503131c78c8aa5f6eb0436a681ff
Yuksel Ramis (BuP2/TEF6) <ramis.yuksel@boschrexroth.com.tr>
2023-07-05 19:51:45 +0300

    create product filter andf validations are created

--- Presentation/ECommerceAPI.WebAPI/Controllers/ProductController.cs
+++ Presentation/ECommerceAPI.WebAPI/Controllers/ProductController.cs
@@ -25,7 +25,15 @@ namespace ECommerceAPI.WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(_productReadRepository.GetAll(false));
+            return Ok(_productReadRepository.GetAll(false).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }));
         }
 
         [HttpGet("{id}")]
--- Presentation/ECommerceAPI.WebAPI/Program.cs
+++ Presentation/ECommerceAPI.WebAPI/Program.cs
@@ -1,10 +1,15 @@
+using ECommerceAPI.Application.Validators.Products;
+using ECommerceAPI.Infrastructure.Filters;
 using ECommerceAPI.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
